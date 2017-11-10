9071816c3227e22ecf2ba8b7baf8bfa967c96cc1
Abhishek Tiwari <abhsihek.193@gmail.com>
2017-11-10 22:24:30 +0530

    email survey app
    sends email survey

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cookieSession = require('cookie-session');
 const passport = require('passport');
 const bodyParser = require('body-parser');
 require('./models/User');
+require('./models/Survey');
 require('./services/passport');
 
 
@@ -32,6 +33,7 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
+require('./routes/surveyRoutes')(app);
 
 if(process.env.NODE_ENV === 'production' ) {
   //express will serve up production assets
